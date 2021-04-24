f60f9964eafd3fe6be185e1593e5dda740b71084
ViniciusMacedo <vini.macedo.c@gmail.com>
2021-04-24 14:07:07 -0300

    Mais campos adicionados em GetLocation Activity

--- app/src/main/java/com/vmc/getmylocation/GetLocation.java
+++ app/src/main/java/com/vmc/getmylocation/GetLocation.java
@@ -2,14 +2,30 @@ package com.vmc.getmylocation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class GetLocation extends AppCompatActivity {
+    private EditText latitude, longitude;
+    private Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_location);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+
+        search = findViewById(R.id.search);
+        search.setOnClickListener(v -> {
+
+        });
+
     }
 }
