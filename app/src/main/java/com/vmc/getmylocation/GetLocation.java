0330417aada697fc38423a405d546b44e3da9673
ViniciusMacedo <vini.macedo.c@gmail.com>
2021-04-24 11:20:40 -0300

    AppBar removed from HomeActivity | BackButton added for GetLocation Activity

--- app/src/main/java/com/vmc/getmylocation/GetLocation.java
+++ app/src/main/java/com/vmc/getmylocation/GetLocation.java
@@ -10,5 +10,6 @@ public class GetLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_location);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
