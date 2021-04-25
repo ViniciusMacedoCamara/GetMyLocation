c6192effbee1f10e5782e26f5044762b1c9200bc
ViniciusMacedo <vini.macedo.c@gmail.com>
2021-04-25 12:36:57 -0300

    error dialog mostrando as falhas cometidas pelo usuario

--- app/src/main/java/com/vmc/getmylocation/activity/GetLocationActivity.java
+++ app/src/main/java/com/vmc/getmylocation/activity/GetLocationActivity.java
@@ -3,11 +3,9 @@ package com.vmc.getmylocation.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -33,40 +31,39 @@ public class GetLocationActivity extends AppCompatActivity {
 
         search = findViewById(R.id.btn_search);
         search.setOnClickListener(v -> {
-            Log.d("latitude", latitude.getText().toString());
-            Log.d("longitude", longitude.getText().toString());
-            Log.d("label", label.getText().toString());
 
-            String canContinue = utils.checkAllFields(latitude.getText().toString(), longitude.getText().toString(), label.getText().toString());
+            String checkFields = utils.checkAllFields(latitude.getText().toString(), longitude.getText().toString(), label.getText().toString());
 
 
-            if (!canContinue.equals("false") && !canContinue.equals("Campos invalidos")){
-                Log.d(tag, "seguir e levar pro google maps");
+            if (!checkFields.equals("false") && !checkFields.equals("Campos invalidos")){
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("geo:0,0?q=" + canContinue));
+                intent.setData(Uri.parse("geo:0,0?q=" + checkFields));
                 Intent chooser = Intent.createChooser(intent, "Lauch");
                 startActivity(chooser);
             } else {
-                Log.d(tag, "Deu ruim!");
-                new AlertDialog.Builder(this)
-                        .setTitle("Delete entry")
-                        .setMessage("Are you sure you want to delete this entry?")
-
-                        // Specifying a listener allows you to take an action before dismissing the dialog.
-                        // The dialog is automatically dismissed when a dialog button is clicked.
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Continue with delete operation
-                            }
-                        })
-
-                        // A null listener allows the button to dismiss the dialog and take no further action.
-                        .setNegativeButton(android.R.string.no, null)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
+                errorDialog(checkFields);
             }
 
         });
 
     }
+
+    public void errorDialog(String errDesc) {
+        String title = "Erro";
+        String message = "Erro desconhecido, favor entrar em contato com o desenvolvedor!";
+        if (errDesc.equals("false")){
+            title = "Campos Não Preenchidos";
+            message = "Favor verificar os campos Latitude e Longitude, eles são obrigatórios!";
+        } else {
+            title = "Campos Inválidos";
+            message = "Os campos Latitude e Longitude foram preenchidos de forma errada, favor verificar sua escrita!";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setNegativeButton(android.R.string.yes, null)
+                .show();
+    }
+
 }
